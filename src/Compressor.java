e87dbaf3fe6902f7df6057b182620b07c89ebad9
alexandre.perez.josende <alexandre.perez.josende@est.fib.upc.edu>
2019-10-05 20:19:11 +0200

    main

---
+++ src/Compressor.java
@@ -0,0 +1,7 @@
+public class Compressor {
+    public static void main(String[] args) throws Exception {
+        // Test
+        //LZ78 alg = new LZ78();
+        //alg.compress(System.in, System.out);
+    }
+}
