6f5e6f4f8e485806bc3c0a4e48467a2c52129fa3
alexandre.perez.josende <alexandre.perez.josende@est.fib.upc.edu>
2019-11-01 09:40:33 +0100

    JPEG refactor and full support for ppm image format

--- src/main/java/Compressor/JPEG.java
+++ src/main/java/Compressor/JPEG.java
@@ -1,8 +1,9 @@
 package Compressor;
 
-import Constants.QuantizationTables;
 import Constants.JPEG_Quality;
 import java.io.*;
+import Utils.Color;
+import Utils.PPMTranslator;
 
 public class JPEG {
     private final int[][] LuminanceQuantizationTable; // Table related to the amount of compression and quality on light intensity
@@ -14,69 +15,55 @@ public class JPEG {
     private int height = -1; // image height
     private int m_width = -1; // number of horizontal 8x8 matrices
     private int m_height = -1; // number of vertical 8x8 matrices
-    private byte[] ppmMode; // P3 or P6
-    private int ppmMaxValue = 255; // R/G/B between [0, ppmMaxValue]
-    
 
     public JPEG(JPEG_Quality quality) {
         LuminanceQuantizationTable = quality.getLuminanceTable();
         ChrominanceQuantizationTable = quality.getChrominanceTable();
     }
 
+    public void compress(InputStream is, OutputStream os) throws IOException {
+        PPMTranslator ppmfile = new PPMTranslator(is);
+        width = ppmfile.getWidth();
+        height = ppmfile.getHeight();
 
-    public void compress (InputStream is, OutputStream os) throws IOException {
-        // Uncompressed header decodification
-        int next;
-        ppmMode = new byte[2];
-        if ((next = is.read()) < 0) throw new IOException("Wrong ppm codification! - Empty file"); // 'P'
-        ppmMode[0] = (byte)next;
-        if ((next = is.read()) < 0) throw new IOException("Wrong ppm codification! - File too short"); // '3' or '6'
-        ppmMode[1] = (byte)next;
-        char c = (char)next;
-        if (c == '3') {} // nice! ascii ppm
-        else if (c == '6') throw new IOException("Binary ppm currently not supported!");
-        else throw new IOException("Wrong ppm codification! - Failed reading ascii/binary");
-
-        if ((width = read_next_number(is)) < 0) throw new IOException("Wrong ppm codification! - Failed reading width");
-        if ((height = read_next_number(is)) < 0) throw new IOException("Wrong ppm codification! - Failed reading height");
-        if ((ppmMaxValue = read_next_number(is)) < 0) throw new IOException("Wrong ppm codification! - Failed reading max value"); // R/G/B between [0, ppmMaxValue]
-        
-        // 8x8 Matrix subdivision (uses margin at the image right and down borders if height or width arent multiple of 8)
-        m_width = width/8;
-        if (width%8 != 0) m_width = m_width + 1;
-        m_height = height/8;
-        if (height%8 != 0) m_height = m_height + 1;
+        // 8x8 Matrix subdivision (uses margin at the image right and down borders if
+        // height or width arent multiple of 8)
+        m_width = width / 8;
+        if (width % 8 != 0)
+            m_width = m_width + 1;
+        m_height = height / 8;
+        if (height % 8 != 0)
+            m_height = m_height + 1;
 
         M0 = new int[m_height][m_width][8][8];
         M1 = new int[m_height][m_width][8][8];
         M2 = new int[m_height][m_width][8][8];
-        
+
         Color color = new Color();
         int i = 0;
         int j = 0;
         int k = 0;
         int l = 0;
-        while ((color = read_next_pixel(is)) != null) {
+        while ((color = ppmfile.getNextColor()) != null) {
             M0[i][j][k][l] = color.Y - 128;
             M1[i][j][k][l] = color.Cb - 128;
             M2[i][j][k][l] = color.Cr - 128;
-            l = l+1;
+            l = l + 1;
             if (l >= 8) {
                 l = 0;
-                k = k+1;
+                k = k + 1;
                 if (k >= 8) {
                     k = 0;
-                    j = j+1;
+                    j = j + 1;
                     if (j >= m_width) {
                         j = 0;
                         i = i + 1;
-                        if (i >= m_height) break;
+                        if (i >= m_height)
+                            break;
                     }
                 }
             }
         }
-
-        
 
         for (i = 0; i < m_height; ++i)
             for (j = 0; j < m_width; ++j) {
@@ -87,26 +74,21 @@ public class JPEG {
                 float[][] dct2 = DCT(M2[i][j]);
                 Quantization(dct2, M2[i][j], ChrominanceQuantizationTable);
             }
-        
-        
-        
-        // WRITE 8x8 MATRIX TO OUTPUT COMPRESSING EACH WITH HUFFMAN
-            // WIP
 
+        // WRITE 8x8 MATRIX TO OUTPUT COMPRESSING EACH WITH HUFFMAN
+        // WIP
 
         os.flush();
     }
 
-    public void decompress (InputStream is, OutputStream os) throws IOException {
+    public void decompress(InputStream is, OutputStream os) throws IOException {
         // READ:
-            // It should read from is, decompress each 8x8 matrix into the internal state using huffman
-            // WIP
+        // It should read from is, decompress each 8x8 matrix into the internal state
+        // using huffman
+        // WIP
         // Create decompressed image using the internal state:
-        // write ppm header
-        os.write(ppmMode);
-        String str = '\n' + String.valueOf(width) + ' ' + String.valueOf(height) + '\n' + String.valueOf(255) + '\n';
-        byte[] b = str.getBytes();
-        os.write(b);
+        PPMTranslator ppmFile = new PPMTranslator(os, width, height);
+        ppmFile.writeHeader();
 
         for (int i = 0; i < m_height; ++i)
             for (int j = 0; j < m_width; ++j) {
@@ -125,92 +107,45 @@ public class JPEG {
         Color color = new Color();
         for (int i = 0; i < m_height; ++i) {
             m_x = 8;
-            if ((i+1)*8 > height) m_y = m_height*8 - height;
+            if ((i + 1) * 8 > height)
+                m_y = m_height * 8 - height;
             for (int j = 0; j < m_width; ++j) {
-                if ((j+1)*8 > width) m_x = m_width*8 - width;
+                if ((j + 1) * 8 > width)
+                    m_x = m_width * 8 - width;
                 for (int k = 0; k < m_y; ++k) {
                     for (int l = 0; l < m_x; ++l) {
                         int Y = M0[i][j][k][l] + 128;
                         int Cb = M1[i][j][k][l] + 128;
                         int Cr = M2[i][j][k][l] + 128;
                         color.YCbCr(Y, Cb, Cr);
-                        write_next_number(os, Math.max(Math.min(255, color.r), 0));
-                        write_next_number(os, Math.max(Math.min(255, color.g), 0));
-                        write_next_number(os, Math.max(Math.min(255, color.b), 0));
+                        ppmFile.setNextComponent(Math.max(Math.min(255, color.r), 0));
+                        ppmFile.setNextComponent(Math.max(Math.min(255, color.g), 0));
+                        ppmFile.setNextComponent(Math.max(Math.min(255, color.b), 0));
                     }
                 }
             }
         }
-    
-
+        
+        os.flush();
     }
-
-
-    private Color read_next_pixel (InputStream is) throws IOException {
-        int r,g,b;
-        if ((r = read_next_number(is)) < 0) return null;
-        if ((g = read_next_number(is)) < 0) throw new IOException("Wrong ppm codification! - Incomplete RGB color");
-        if ((b = read_next_number(is)) < 0) throw new IOException("Wrong ppm codification! - Incomplete RGB color");
-        float pixelDepthFactor = (float)(255.0/(float)ppmMaxValue);
-        Color c = new Color();
-        float r_f,g_f,b_f;
-        r_f = Math.min(r*pixelDepthFactor, 255);
-        g_f = Math.min(g*pixelDepthFactor, 255);
-        b_f = Math.min(b*pixelDepthFactor, 255);
-        c.RGB(r_f, g_f, b_f);
-        return c;
-    }
-
-    private int read_next_number (InputStream is) throws IOException {
-        int next;
-        int num = 0;
-        boolean numberRead = false;
-        while ((next = is.read()) >= 0) {
-            char c = (char)next;
-            if ((c == '\n' || c == ' ') && numberRead) return num; // reached end of number
-            if (c == '\n' || c == ' ') continue; // avoid excesive '\n'
-            if (c == '#') { // avoid comments
-                if (numberRead) return num; // number contains comment w/o space, return current num
-                while ((next = is.read()) >= 0){
-                    c = (char)next;
-                    if (c == '\n') break; // comment finish
-                }
-                if (c != '\n') return -1; // no number, just EOF
-                continue;
-            }
-            num = num * 10;
-            num = num + Character.getNumericValue(c);
-            numberRead = true;
-        }
-        if (numberRead) return num; // reached end of number at end of file
-        return -1; // no number, just EOF
-    }
-
-    private void write_next_number (OutputStream os, int num) throws IOException {
-        String str = String.valueOf(num) + '\n';
-        byte[] b = str.getBytes();
-        os.write(b);
-    }
-    
 
     private void Quantization(float[][] input, int[][] output, int[][] QuantizationTable) { // factor between 0 and +infinity
         for (int i = 0; i < 8; ++i)
-            for  (int j = 0; j < 8; ++j) {
-                output[i][j] = (int)(input[i][j] / QuantizationTable[i][j]);
+            for (int j = 0; j < 8; ++j) {
+                output[i][j] = (int) (input[i][j] / QuantizationTable[i][j]);
             }
     }
 
     private void Dequantization(int[][] input, float[][] output, int[][] QuantizationTable) { // factor between 0 and +infinity
         for (int i = 0; i < 8; ++i)
-            for  (int j = 0; j < 8; ++j) {
-                output[i][j] = (float)(input[i][j] * QuantizationTable[i][j]);
+            for (int j = 0; j < 8; ++j) {
+                output[i][j] = (float) (input[i][j] * QuantizationTable[i][j]);
             }
     }
 
-
     private float[][] initDCTCoefficients(float[][] c) {
         final int N = c.length;
-        final float value = (float)(1 / Math.sqrt(2.0));
+        final float value = (float) (1 / Math.sqrt(2.0));
 
         for (int i = 1; i < N; i++) {
             for (int j = 1; j < N; j++) {
@@ -226,10 +161,9 @@ public class JPEG {
         return c;
     }
 
-    
     private float[][] DCT(int[][] input) {
         final int N = input.length;
-        final float mathPI = (float)(Math.PI);
+        final float mathPI = (float) (Math.PI);
         final int halfN = N / 2;
         final float doubN = 2.f * N;
 
@@ -257,10 +191,9 @@ public class JPEG {
         return output;
     }
 
-    
     private int[][] IDCT(float[][] input) {
         final int N = input.length;
-        final float mathPI = (float)(Math.PI);
+        final float mathPI = (float) (Math.PI);
         final int halfN = N / 2;
         final float doubN = 2.f * N;
 
@@ -282,32 +215,10 @@ public class JPEG {
                     }
                 }
                 sum /= halfN;
-                output[x][y] = (int)sum;
+                output[x][y] = (int) sum;
             }
         }
         return output;
     }
-    
-}
 
-class Color {
-    int r,g,b;
-    int Y,Cb,Cr;
-    public void RGB(float r, float g, float b) { // uses float instead of int to reduce aproximation imprecision
-        this.r = (int)r;
-        this.g = (int)g;
-        this.b = (int)b;
-        Y = (int)(float)((0.299 * r) + (0.587 * g) + (0.114 * b));
-        Cb = (int)(float)(128.0 - (0.168736 * r) - (0.331264 * g) + (0.5 * b));
-        Cr = (int)(float)(128.0 + (0.5 * r) - (0.418688 * g) - (0.081312 * b));
-    }
-    
-    public void YCbCr(int Y, int Cb, int Cr){
-        this.Y = Y;
-        this.Cb = Cb;
-        this.Cr = Cr;
-        r = (int)(float)(Y + 1.402 * (Cr - 128.0));
-        g = (int)(float)(Y - 0.344136 * (Cb - 128.0) - 0.714136 * (Cr - 128.0));
-        b = (int)(float)(Y + 1.772 * (Cb - 128.0));
-    }
 }
---
+++ src/main/java/Utils/PPMTranslator.java
@@ -0,0 +1,152 @@
+package Utils;
+
+import java.io.*;
+import Utils.Color;
+
+public class PPMTranslator {
+    PPMType encoding;
+    InputStream is = null;
+    OutputStream os = null;
+    int height, width, maxValue;
+
+    public PPMTranslator(InputStream is) throws IOException {
+        this.is = is;
+        extractHeader();
+    }
+
+    public PPMTranslator(OutputStream os, int width, int height) throws IOException {
+        this.os = os;
+        this.width = width;
+        this.height = height;
+        maxValue = 255;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getNextComponent() throws IOException {
+        assert (is != null);
+        int num = 0;
+        int next;
+        if (encoding == PPMType.ASCII) {
+            char c;
+            while ((next = is.read()) >= 0) {
+                c = (char) next;
+                if (c == '\n')
+                    return num;
+                num = num * 10 + Character.getNumericValue(c);
+            }
+        } else if ((next = is.read()) >= 0)
+            return next;
+        return -1;
+    }
+
+    public Color getNextColor() throws IOException {
+        int r, g, b;
+        if ((r = getNextComponent()) < 0)
+            return null;
+        if ((g = getNextComponent()) < 0)
+            throw new IOException("Wrong ppm codification! - Incomplete RGB color");
+        if ((b = getNextComponent()) < 0)
+            throw new IOException("Wrong ppm codification! - Incomplete RGB color");
+        float pixelDepthFactor = (float) (255.0 / (float) maxValue);
+        float r_f, g_f, b_f;
+        r_f = Math.min(r * pixelDepthFactor, 255);
+        g_f = Math.min(g * pixelDepthFactor, 255);
+        b_f = Math.min(b * pixelDepthFactor, 255);
+        Color c = new Color();
+        c.RGB(r_f, g_f, b_f);
+        return c;
+    }
+
+    public void setNextComponent(int num) throws IOException {
+        if (encoding == PPMType.ASCII) {
+            String str = String.valueOf(num) + '\n';
+            byte[] b = str.getBytes();
+            os.write(b);
+        } else
+            os.write(num);
+    }
+
+    public void setNextColor(Color color) throws IOException {
+        setNextComponent(color.r);
+        setNextComponent(color.g);
+        setNextComponent(color.b);
+    }
+
+    private void extractHeader() throws IOException {
+        int next;
+        if ((next = is.read()) < 0)
+            throw new IOException("Wrong ppm codification! - Empty file"); // 'P'
+        if ((next = is.read()) < 0)
+            throw new IOException("Wrong ppm codification! - File too short"); // '3' or '6'
+        char c = (char) next;
+        if (c == '3')
+            encoding = PPMType.ASCII;
+        else if (c == '6')
+            encoding = PPMType.Raw;
+        else
+            throw new IOException("Wrong ppm codification! - Failed reading ascii/binary flag");
+
+        if ((width = readHeaderNum()) < 0)
+            throw new IOException("Wrong ppm codification! - Failed reading width");
+        if ((height = readHeaderNum()) < 0)
+            throw new IOException("Wrong ppm codification! - Failed reading height");
+        if ((maxValue = readHeaderNum()) < 0)
+            throw new IOException("Wrong ppm codification! - Failed reading max value");
+    }
+
+    private int readHeaderNum() throws IOException {
+        int next;
+        int num = 0;
+        boolean numberRead = false;
+        while ((next = is.read()) >= 0) {
+            char c = (char) next;
+            if ((c == '\n' || c == ' ') && numberRead)
+                return num; // reached end of number
+            if (c == '\n' || c == ' ')
+                continue; // avoid excesive '\n'
+            if (c == '#') { // avoid comments
+                if (numberRead)
+                    return num; // number contains comment w/o space, return current num
+                while ((next = is.read()) >= 0) {
+                    c = (char) next;
+                    if (c == '\n')
+                        break; // comment finish
+                }
+                if (c != '\n')
+                    return -1; // no number, just EOF
+                continue;
+            }
+            num = num * 10;
+            num = num + Character.getNumericValue(c);
+            numberRead = true;
+        }
+        if (numberRead)
+            return num; // reached end of number at end of file
+        return -1; // no number, just EOF
+    }
+
+    public void writeHeader() throws IOException {
+        this.encoding = PPMType.Raw;
+        String type;
+        if (encoding == PPMType.ASCII)
+            type = "P3";
+        else
+            type = "P6";
+        String str = type + '\n' + String.valueOf(width) + ' ' + String.valueOf(height) + '\n' + String.valueOf(255)
+                + '\n';
+        byte[] header = str.getBytes();
+        os.write(header);
+    }
+
+}
+
+enum PPMType {
+    ASCII, Raw
+}
