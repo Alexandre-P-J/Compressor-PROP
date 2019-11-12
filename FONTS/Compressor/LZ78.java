ac4a25ad8c59a091c164a29b1816fc3193e46dc5
itsSkyRex <a.ibars99@gmail.com>
2019-11-12 16:26:22 +0100

    Documented

--- FONTS/Compressor/LZ78.java
+++ FONTS/Compressor/LZ78.java
@@ -4,7 +4,10 @@ import Container.ByteArray;
 import Container.Dictionary;
 import IO.BitInputStream;
 import IO.BitOutputStream;
-import java.io.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * @author Daniel Clemente
@@ -12,73 +15,75 @@ import java.io.*;
 
 public class LZ78 {
     class Code {
-        int c;          // The next char
+        int c;          // The next byte/character
         int code;       // The code
         Code (int code, int c) {
             this.c = c; this.code = code;
         }
     };
 
-    int nBits;
     Dictionary dict;
-
-    byte[] buff;
-
-    ByteArray emptyAB = new ByteArray();
-    ByteArray ab = emptyAB;
+    // The number of bits that should be written for each code
+    int nBits;
+    
+    // The previous byte array that we should remember
+    // in order to instert into the dictionary.
+    ByteArray emptyBA = new ByteArray();
+    ByteArray ba = emptyBA;
     
     /**
-     * Encodes the next character.
-     * @param n the character to encode.
+     * Encodes the next byte.
+     * @param n the byte to encode.
      * @return the code generated, if not returns -1.
      */
-    int codifyChar (int n) {
+    private int encodeByte (int n) {
         byte b = (byte)n;
-        ByteArray aux = ab.concatenate(b);
+        ByteArray aux = ba.concatenate(b);
         int code = dict.getNumStr(aux);
+        // if it exists then we continue searching for a longer byte array
         if (code != -1) {
-            ab = aux;
+            ba = aux;
             return -1;
         }
         else {
             dict.add(aux);
-            aux = ab;
-            ab = emptyAB;
+            aux = ba;
+            ba = emptyBA;
             return dict.getNumStr(aux);
         }
     }
 
     /**
-     * If there something left in ab, encode it.
+     * Encode de last byte of the sequence if there is something left.
      * @return the code left.
      */
-    Code codifyLast () {
-        if (ab.size() == 0) return null;
-        byte b = ab.getLastByte();
-        ab = ab.dropLast();
+    private Code encodeLastByte () {
+        if (ba.size() == 0) return null;
+        byte b = ba.getLastByte();
+        ba = ba.dropLast();
         int next = b & 0xFF;
-        return new Code(dict.getNumStr(ab), next);
+        return new Code(dict.getNumStr(ba), next);
     }
 
     /**
      * Call the write function with the necessary bits to write the code.
-     * @param bos the BitOutputStream.
+     * @param bos the BitOutputStream to write of.
      * @param co the code to write.
-     * @throws IOException If there is a problem.
+     * @throws IOException if wrtting to the output stream fails.
      */
-    void writeCode (BitOutputStream bos, Code co) throws IOException {
+    private void writeCode (BitOutputStream bos, Code co) throws IOException {
 		writeCode(bos,co.c,8);
 		writeCode(bos,co.code,nBits);
     }
     
     /**
-     * Write the code in bits into output stream.
-     * @param bos the BitOutputStream.
+     * Write the code in bits into output stream with the help of the BitOutputStream.
+     * @param bos the BitOutputStream the write of.
      * @param n the code to write.
      * @param bits number of bits from the code.
-     * @throws IOException If there is a problem.
+     * @throws IOException if wrtting to the output stream fails.
      */
-    void writeCode (BitOutputStream bos, int n, int bits) throws IOException {
+    private void writeCode (BitOutputStream bos, int n, int bits) throws IOException {
 		for (int i = 0; i < bits; ++i) {
 			bos.write1Bit(n&1);
 			n = n / 2;
@@ -86,12 +91,12 @@ public class LZ78 {
     }
     
     /**
-     * 
-     * @param bis
-     * @return
-     * @throws IOException
+     * Read the code from the given bit input stream, and returns it as a Code.
+     * @param bis the BitInputStream to read of.
+     * @return the code of nBits gereneted from the input stream.
+     * @throws IOException if reading from the input stream fails.
      */
-    Code readCode (BitInputStream bis) throws IOException { 
+    private Code readCode (BitInputStream bis) throws IOException { 
         int ch = readInt(bis,8);
         if (ch < 0) return null;
         int co = readInt(bis,nBits);
@@ -100,13 +105,13 @@ public class LZ78 {
     }
 
     /**
-     * 
-     * @param bis
-     * @param bits
-     * @return
-     * @throws IOException
+     * Read the code from the given bit input stream, and returns it as an int.
+     * @param bis the BitInputStream to read of.
+     * @param bits the number of bits of the code.
+     * @return an Integer with the code generated from the input stream.
+     * @throws IOException if reading from the input stream fails.
      */
-    int readInt (BitInputStream bis, int bits) throws IOException {
+    private int readInt (BitInputStream bis, int bits) throws IOException {
 		int n = 0;
 		for (int i=0;i < bits; ++i) {
 			int next = bis.read1Bit();
@@ -117,29 +122,29 @@ public class LZ78 {
     }
 
     /**
+     * Creates a new dictionary with maximum the size of DictBitSize.
      * Compresses the given input stream, writing to the given output stream.
-     * @param is the InputStream.
-     * @param os the OutputStream.
+     * @param is the input stream to read data.
+     * @param os the output stream to save data..
      * @param DictBitSize Dictionary size.
-     * @throws Exception If cannot read/write files.
+     * @throws Exception if reading or writting to a stream fails.
      */
     public void compress (InputStream is, OutputStream os, int DictBitSize) throws Exception {
         if (DictBitSize > 31 || DictBitSize < 0) throw new IllegalArgumentException("Dict size must be between 2^0 and 2^31 !");
         nBits = DictBitSize;
-        buff = new byte[nBits];
         dict = new Dictionary(1<<nBits);
-        dict.add(emptyAB);
+        dict.add(emptyBA);
         os.write(nBits); // Write DictBitSize to the compressed stream
 
         BitOutputStream bos = new BitOutputStream(os);
-
-        int code;
-        int next;
+        int code;   // next input byte
+        int next;   // next code generated
         while ((next = is.read()) >= 0){
-            code = codifyChar(next);
+            code = encodeByte(next);
             if (code >= 0) writeCode(bos, new Code(code,next));
         }
-        Code co = codifyLast();
+        // If there something left in ba
+        Code co = encodeLastByte();
         if (co != null) writeCode(bos, co);
         bos.flush();
     }
@@ -147,30 +152,31 @@ public class LZ78 {
     /**
      * Decodes the next code.
      * @param co the code to decode.
-     * @return
+     * @return a ByteArray with the code decoded.
      */
-    ByteArray disarray (Code co) {
+    private ByteArray disarray (Code co) {
         ByteArray aux = dict.getStrNum(co.code);
         dict.add(aux.concatenate((byte)co.c));
         return aux;
     }
 
     /**
+     * Creates a new dictionary of the sized received in the first input stream byte.
      * Decompresses the given input stream, writing to the given output stream.
-     * @param is the InputStream.
-     * @param os the OutputStream.
-     * @throws Exception If cannot read/write files.
+     * @param is the input stream to read data.
+     * @param os the output stream to write data
+     * @throws Exception if reading or writting to a stream fails.
      */
     public void decompress (InputStream is, OutputStream os) throws Exception {
         nBits = is.read();
         if (nBits > 31 || nBits < 0) throw new IllegalArgumentException("Dict size must be between 2^0 and 2^31 !");
-        buff = new byte[nBits];
+        // Create a new dictionary with maximum of 2^bits entrie
         dict = new Dictionary(1<<nBits);
-        dict.add(emptyAB);
+        dict.add(emptyBA);
 
         BitInputStream bis = new BitInputStream(is);
-        ByteArray s;
-        Code co;
+        ByteArray s;    // Next entry
+        Code co;        // Next code to be read
         while ((co = readCode(bis)) != null) {
             s = disarray(co);
             os.write(s.getBytes());
--- FONTS/Container/ByteArray.java
+++ FONTS/Container/ByteArray.java
@@ -1,10 +1,10 @@
 package Container;
 
 public class ByteArray {
-    final byte[] array;
+    final private byte[] array;
 
     /**
-     * Default constructor, 0 length array
+     * Default constructor, 0 length array.
      */
     public ByteArray() {
         array = new byte[0];
@@ -12,31 +12,31 @@ public class ByteArray {
 
     /**
      * Constructor with a single byte.
-     * @param b
+     * @param b the byte to be contained in the new byte array.
      */
     public ByteArray(byte b) {
         array = new byte[] { b };
     }
 
     /**
-     * Cloning constructor
-     * @param ab
+     * Cloning constructor.
+     * @param ba the byte array to be cloned.
      */
-    public ByteArray(byte[] ab) {
-        array = (byte[]) ab.clone();
+    public ByteArray(byte[] ba) {
+        array = (byte[]) ba.clone();
     }
 
     /**
      * Returns the size of the byte array.
-     * @return the size.
+     * @return the size as a Integer.
      */
     public int size() {
         return array.length;
     }
 
     /**
-     * For the hash-table we need this.
-     * @return 
+     * Compares the array of an object with the main byte array. Need it for the hash-table.
+     * @return true if the two arrays are equal, otherwise false.
      */
     public boolean equals(Object o) {
         ByteArray ba = (ByteArray) o;
@@ -44,8 +44,8 @@ public class ByteArray {
     }
 
     /**
-     * For the hash-table we need to give a hash code.
-     * @return 
+     * Hash code for the hash-table.
+     * @return the code of the hash-table.
      */
     public int hashCode() {
         int code = 0;
@@ -56,7 +56,7 @@ public class ByteArray {
 
     /**
      * Returns the byte in a given position.
-     * @param p position 
+     * @param p position to get the byte of.
      * @return the byte in the position p.
      */
     public byte getBytePos(int p) {
@@ -65,21 +65,21 @@ public class ByteArray {
 
     /**
      * Concatenates another byte array into this one.
-     * @param ab the ByteArray to concatenate.
+     * @param ba the ByteArray to concatenate.
      * @return the concatenation in another newly created one.
      */
-    public ByteArray concatenate(ByteArray ab) {
-        int n = size() + ab.size();
+    public ByteArray concatenate(ByteArray ba) {
+        int n = size() + ba.size();
         byte[] b = new byte[n];
         for (int i = 0; i < size(); ++i)
             b[i] = getBytePos(i);
-        for (int i = 0; i < ab.size(); ++i)
-            b[i + size()] = ab.getBytePos(i);
+        for (int i = 0; i < ba.size(); ++i)
+            b[i + size()] = ba.getBytePos(i);
         return new ByteArray(b);
     }
 
     /**
-     * Concatenates a byte in this array of bytes
+     * Concatenates a byte in this array of bytes.
      * @param b the byte to concatenate.
      * @return the concatenations in another newly created one.
      */
@@ -87,7 +87,12 @@ public class ByteArray {
         return concatenate(new ByteArray(b));
     }
 
-    // Retorna el ByteArray entre [beginIndex, endIndex)
+    /**
+     * Returns the ByteArray between [beginIndex, endIndex].
+     * @param beginIndex the first desired index.
+     * @param endIndex the final desired index.
+     * @return the ByteArray between [beginIndex, endIndex].
+     */
     public ByteArray subByteArray(int beginIndex, int endIndex) {
         assert (beginIndex >= 0);
         assert (endIndex >= beginIndex);
@@ -98,7 +103,12 @@ public class ByteArray {
         return new ByteArray(bar);
     }
 
-    // Retorna l'index de la primera ocurrencia del ByteArray ba dins d'aquest ByteArray
+    
+    /**
+     * Returns the index of the first ocurrence of ByteArray ba in this ByteArray.
+     * @param ba the ByteArray to look for.
+     * @return the index, otherwise returns -1.
+     */
     public int indexOf(ByteArray ba) {
         if (ba.size() == 0)
             return -1;
@@ -113,7 +123,12 @@ public class ByteArray {
         return -1;
     }
 
-    // Retorna un ByteArray que ha eliminat els valors compresos entre [beginIndex, endIndex)
+    /**
+     * Returns a ByteArray without the compressed values between [beginIndex, endIndex).
+     * @param beginIndex the first desired index.
+     * @param endIndex the final desired index.
+     * @return a ByteArray that has eliminated the values between [beginIndex, endIndex).
+     */
     public ByteArray delete(int beginIndex, int endIndex) {
         assert (beginIndex >= 0);
         assert (endIndex >= 0);
@@ -134,7 +149,7 @@ public class ByteArray {
     }
 
     /**
-     * Drops the last character.
+     * Drops the last byte.
      * @return the last byte of the ByteArray.
      */
     public byte getLastByte() {
@@ -142,8 +157,8 @@ public class ByteArray {
     }
 
     /**
-     * 
-     * @return
+     * Returns the ByteArray with the last byte drop.
+     * @return the ByteArray with the last byte drop.
      */
     public ByteArray dropLast() {
         byte[] arr = new byte[size() - 1];
