283aec44b153290e866b044390016569b91ad79f
alexandre.perez.josende <alexandre.perez.josende@est.fib.upc.edu>
2019-11-15 15:41:25 +0100

    Test alert

--- FONTS/Testing/ExtraTestsDriver.java
+++ FONTS/Testing/ExtraTestsDriver.java
@@ -10,10 +10,11 @@ import ExtraTests.*;
 
 public class ExtraTestsDriver {
         public static void main(String args[]) {
+            System.out.printf("\n[III] INFO: THIS TEST LASTS FOR SEVERAL MINUTES AND CONSUMES A LOT OF SYSTEM ENTROPY, BE PATIENT [III]\n\n\n\n");
             JUnitCore core = new JUnitCore();
             CustomTestListener cl = new CustomTestListener();
             core.addListener(cl);
-            core.run(IOTest.class, HuffmanTest.class);
+            core.run(IOTest.class, LZ78Test.class, LZSSTest.class, LZWTest.class, HuffmanTest.class);
             System.out.printf("\n\nTests Run: %d   Tests Failed: %d   Tests Ignored: %d\n", cl.Started, cl.Failed, cl.Ignored);
         }
 }
