f4258e90d95cd3196e5f9799e4c8480ae264a46d
alexandre.perez.josende <alexandre.perez.josende@est.fib.upc.edu>
2019-12-08 16:35:21 +0100

    typo fix

--- FONTS/Presentation/TextPanel.java
+++ FONTS/Presentation/TextPanel.java
@@ -8,7 +8,7 @@ public class TextPanel extends JPanel{
     private JTextArea textArea;
 
     public TextPanel() {
-        textArea = new JTextArea());
+        textArea = new JTextArea();
         setLayout(new BorderLayout());
         add(new JScrollPane(textArea), BorderLayout.CENTER);
 
